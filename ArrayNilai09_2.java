1962dd7adefe01865c4e91f9f0aff52a508cc016
devitadwtar <144086102+devitadwtar@users.noreply.github.com>
2023-10-24 14:37:53 +0700

    Add files via upload

---
+++ ArrayNilai09_2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArrayNilai09_2 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int[] nilaiAkhir = new int[10];
+        
+
+        for (int i = 0; i < nilaiAkhir.length; i++){
+        System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+        nilaiAkhir [i] = sc.nextInt();
+        }
+        for (int i = 0; i < nilaiAkhir.length; i++){
+            if (nilaiAkhir[i] > 70 ){
+                System.out.println("Mahasiswa ke-"+i+"lulus!");
+           
+            } else if (nilaiAkhir[i] < 70){
+                System.out.println("Mahasiswa ke-"+i+"Tidak lulus");
+            }
+            }
+        }
+    }
